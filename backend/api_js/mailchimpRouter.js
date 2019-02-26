f3aa5996041fa3c7f2efa5dca5f65582eebde3ad
Rd-Feng <394@holbertonschool.com>
2019-02-26 13:52:20 -0800

    add to email list

--- backend/api_js/mailchimpRouter.js
+++ backend/api_js/mailchimpRouter.js
@@ -1,26 +1,45 @@
 const express = require('express');
-const mysql = require('mysql');
+const request = require('superagent');
+const request = require('request');
 const HOST = process.env.HOST;
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const DATABASE = process.env.DATABASE;
-
-function getConnection (){
-  return mysql.createPool({
-    host: HOST,
-    port: 3306,
-    user: USER,
-    password: PASSWORD,
-    database: DATABASE,
-    connectionLimit: 100
-  })
-}
+const mailchimpInstance   = 'us20',
+const listUniqueId        = '38727d257e',
+const mailchimpApiKey     = '85ef0b54b6ca36a2b2050439769f264d-us20';
 
 const router = express.Router();
-const connection = getConnection();
 
 router.get('/mailchimp', (req, res) => {
-  res.send(`Hello from port ${PORT} mailchimp`);
-})
+  res.send(`Hello from port 5001 mailchimp`);
+});
+
+router.POST('/mailchimp/subscribe', (req, res) => {
+  const { email, first_name, last_name } = req.body;
+  request
+  .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+  .set('Content-Type', 'application/json;charset=utf-8')
+  .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+  .send({
+    'email_address': email,
+    'status': 'subscribed',
+    'merge_fields': {
+      'FNAME': first_name,
+      'LNAME': last_name
+    }
+  })
+  .end(function(err, response) {
+    if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+      return res.json({
+        status: 'Success'
+      });
+    } else {
+      return res.json({
+        error: err
+      });
+    }
+  });
+});
 
 module.exports = router;
